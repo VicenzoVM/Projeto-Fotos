cd21015669370a79fb0faf42b1428ba948e0b1b0
Vicenzo <vicenzovalmorbida@outlook.com>
2025-03-17 13:26:14 -0300

    The qrcode reader and indexing by folder is done

--- src/main.py
+++ src/main.py
@@ -1,20 +1,43 @@
 import os
-import pyzbar
+from pyzbar.pyzbar import decode
 import cv2
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 IMAGES_FOLDER = os.path.join(BASE_DIR, "..", "data", "images")
 
+PROCESSED_IMAGES = os.path.join(BASE_DIR, "..", "data", "processed_images")
+
 allowed_extensions = (".jpg", ".jpeg", ".png", ".bmp")
 
 def Check_Files():
-    for image in os.listdir(IMAGES_FOLDER):
+    for image in reversed(os.listdir(IMAGES_FOLDER)):
         if image.lower().endswith(allowed_extensions):
-            return 1
+            qr_code_data = Check_If_QRCODE(image) 
+            
+            if qr_code_data:
+                    qr_code_message = (qr_code_data[0][0]).decode("utf-8")
+                    os.mkdir(os.path.join(PROCESSED_IMAGES,qr_code_message))
+                    os.rename(os.path.join(IMAGES_FOLDER,image),os.path.join(PROCESSED_IMAGES,qr_code_message,image)) 
+                    
+                    
+            else:
+                os.rename(os.path.join(IMAGES_FOLDER,image),os.path.join(PROCESSED_IMAGES,qr_code_message,image)) 
+                
+                    
+                
+                
+                
+                
+                
             
             
+def Check_If_QRCODE(image):
+    image_path = os.path.join(IMAGES_FOLDER, image)
+    converted_image = Image.open(image_path).convert('L')  # 'L' = Grayscale
+    return decode(converted_image)
     
-def Check_If_QRCODE(image_path):
-     image = cv2.imread(str(image_path)) 
-     decode(image)
+
+
+Check_Files()
